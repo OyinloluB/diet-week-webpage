825c4b33d26c1545d6cb442c3f0175e57d1887da
OyinloluB <oyinloluwab@gmail.com>
2020-09-02 04:13:00 -0700

    responsiveness

--- src/components/image-flex/Image.js
+++ src/components/image-flex/Image.js
@@ -2,6 +2,7 @@ import React from "react";
 import Background from "../../assets/image-one.jpg";
 import Styles from "./image.module.css";
 import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
+import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
 
 const Image = () => {
   return (
@@ -16,6 +17,19 @@ const Image = () => {
       <div className={Styles.control}>
         <div>
           <ArrowUpwardIcon style={{ color: "white" }} />
+        </div>
+      </div>
+      <div className={Styles.container}>
+        <h1 className={Styles.hero}>Diet Week</h1>
+        <p className={Styles.subtext}>
+          With 10 different meal plan options, including gluten-free, vegan, and
+          Mediterranean kits, there's something for everyone with Sun Basket
+        </p>
+        <button className={Styles.button}>See more</button>
+      </div>
+      <div className={Styles.control}>
+        <div>
+          <ArrowDownwardIcon style={{ color: "white" }} />
         </div>
       </div>
     </div>
